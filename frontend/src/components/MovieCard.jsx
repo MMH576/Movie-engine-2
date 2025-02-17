a8ce372e4ba50761e7d7a8b652ecf0fb91aa779d
MMH576 <monjorulmohammed@gmail.com>
2025-02-16 23:32:46 -0500

    complete movie card component

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -5,7 +5,30 @@ const MovieCard = ({
 }) => {
   return (
     <div className="movie-card">
-      <p className="text-white">{title}</p>
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : "./no-movie.png"
+        }
+        alt={title}
+      />
+      <div className="mt-4">
+        <div className="content">
+          <div className="rating">
+            <img src="./Rating.svg" alt="star icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+            <span>•</span>
+          </div>
+          <p className="lang">
+            {original_language ? original_language : "N/A"}
+          </p>
+          <span>•</span>
+          <p className="year">
+            {release_date ? release_date.split("-")[0] : "N/AS"}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
